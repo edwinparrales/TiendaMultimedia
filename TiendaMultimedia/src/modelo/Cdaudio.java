7e0110b4b06a1baf9f9e0c3acbece384c9ab6c7e
UsuarioEp <UsuarioEp@UsuarioEp-PC>
2015-03-28 01:12:57 -0500

    cracion de clase cdaudio

---
+++ TiendaMultimedia/src/modelo/Cdaudio.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author UsuarioEp
+ */
+public class Cdaudio extends Producto {
+    private String nomInter,nomCancion;
+
+    public Cdaudio() {
+    }
+
+    public Cdaudio(String cod, String nompro, double precio) {
+        super(cod, nompro, precio);
+    }
+
+    public String getNomInter() {
+        return nomInter;
+    }
+
+    public void setNomInter(String nomInter) {
+        this.nomInter = nomInter;
+    }
+
+    public String getNomCancion() {
+        return nomCancion;
+    }
+
+    public void setNomCancion(String nomCancion) {
+        this.nomCancion = nomCancion;
+    }
+    
+    
+     
+}
