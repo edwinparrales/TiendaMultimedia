4fa09e3d15696f2a8d166a9855bbdf0f58fdfe9d
UsuarioEp <UsuarioEp@UsuarioEp-PC>
2015-03-30 19:51:57 -0500

    Creacion de los demas  productos implementando arraylist en lugar de vectores

---
+++ TiendaMultimedia/src/controla/ArrayCdmp3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controla;
+
+import java.util.ArrayList;
+import modelo.Cdmp3;
+
+/**
+ *
+ * @author UsuarioEp
+ */
+public class ArrayCdmp3 {
+   ArrayList<Cdmp3>arrmp3=new ArrayList<Cdmp3>();
+   ArrayList<Cdmp3>aux=new ArrayList<Cdmp3>();
+
+    public ArrayCdmp3() {
+       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
+       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
+       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
+       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
+       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
+        
+    }
+    
+    
+    
+}
