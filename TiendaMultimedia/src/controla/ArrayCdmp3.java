2d606f257d257a0e51433d37fdc449d8444924a9
UsuarioEp <UsuarioEp@UsuarioEp-PC>
2015-03-30 20:39:18 -0500

    Creacion de los demas  productos implementando arraylist en lugar de vectores

--- TiendaMultimedia/src/controla/ArrayCdmp3.java
+++ TiendaMultimedia/src/controla/ArrayCdmp3.java
@@ -13,18 +13,24 @@ import modelo.Cdmp3;
  * @author UsuarioEp
  */
 public class ArrayCdmp3 {
+    
    ArrayList<Cdmp3>arrmp3=new ArrayList<Cdmp3>();
    ArrayList<Cdmp3>aux=new ArrayList<Cdmp3>();
 
     public ArrayCdmp3() {
        arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
-       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
-       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
-       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
-       arrmp3.add(new Cdmp3(" mark antony & La india","vivir lo nuestro"," 1", " Coleccin salsa ", 50000f));
-        
+       arrmp3.add(new Cdmp3("Hector lavo","El cantante "," 2", " Coleccin salsa ", 60000f));
+       arrmp3.add(new Cdmp3(" Ñejo flow"," cuando me dira"," 3", " Coleccin crosover ", 50000f));
+       arrmp3.add(new Cdmp3("Pipe pelaez"," tan natural "," 1", " Coleccin vallenatos ", 150000f));
+       arrmp3.add(new Cdmp3(" Niky jeam "," el perdon","50", " Regueaton ", 60000f));
+         
     }
     
-    
+    public void agregaDisco(){
+        
+        
+        
+        
+    }
     
 }
